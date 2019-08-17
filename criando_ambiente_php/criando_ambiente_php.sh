41b5cf41d0139d8c29f765313af714e5ba159306
Gabriel Cruz <48037228+gabrielpcruz@users.noreply.github.com>
2019-08-16 21:15:26 -0300

    Atualizando para o php7.3

--- criando_ambiente_php/criando_ambiente_php.sh
+++ criando_ambiente_php/criando_ambiente_php.sh
@@ -8,7 +8,7 @@ sudo apt-get install apache2
 
 sudo add-apt-repository -y ppa:ondrej/php && sudo apt-get update
 
-sudo apt-get install php7.3-cli libapache2-mod-php7.3 php7.3-mysql php7.3-curl php-memcached php7.3-dev php7.3-sqlite3 php7.3-mbstring php7.3-gd php7.3-json php7.3-intl
+sudo apt install php7.3-common php7.3-mysql php7.3-xml php7.3-xmlrpc php7.3-curl php7.3-gd php7.3-imagick php7.3-cli php7.3-dev php7.3-imap php7.3-mbstring php7.3-opcache php7.3-soap php7.3-zip php7.3-intl -y
 
 sudo apt install curl
 
@@ -21,5 +21,7 @@ sudo apt-get install mysql-server mysql-client
 sudo apt install mysql-workbench
 
 sudo snap install phpstorm --classic
+
+sudo snap install datagrip --classic
 
 sudo apt install git
